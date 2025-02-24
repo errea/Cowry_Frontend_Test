9af597098f4b63ae0660e07657df982537a49f56
errea <erreakezy@gmail.com>
2025-02-24 18:32:04 +0100

    ADD: basic project configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,24 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt']
+  modules: ['@pinia/nuxt', '@vite-pwa/nuxt',],
+  // pwa: {
+  //   /* your pwa options */
+  // }
+  css: ['~/assets/css/tailwind.css',],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '~/assets/scss/variable.scss',
+        }
+      }
+    },
+  },
 })
