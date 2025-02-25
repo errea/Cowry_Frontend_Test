5da5e6498c5a14e0b15cfdbb3336dccf4a3b1bdf
errea <erreakezy@gmail.com>
2025-02-25 15:15:14 +0100

    ADD: person card and search components

---
+++ components/common/PersonCard.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="w-full h-full absolute top-[16rem] left-0">
+        <div class="max-w-7xl mx-auto">
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+                <div v-for="(column, index) in columns" :key="index"
+                    class="bg-no-repeat relative h-[28rem] w-[22rem] rounded-lg shadow-lg overflow-hidden"
+                    >
+                    <img class="w-full h-full object-cover" :src="column.image" alt="" />
+                    <div class="text-white absolute top-[22rem] pl-2">
+                        <h2 class="text-2xl">{{ column.name }}</h2>
+                        <p class="mt-1">{{ column.location }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const columns = [
+    {
+        name: "Jordan Okeke",
+        location: "Lupus, Nigaria",
+        image: "/images/image-1.jpg",
+    },
+    {
+        name: "Okere Herbert",
+        location: "London, United Kingdom",
+        image: "/images/image-1.jpg",
+    },
+    {
+        name: "Jordan Okeke",
+        location: "Lupus, Nigaria",
+        image: "/images/image-1.jpg",
+    },
+    {
+        name: "Okere Herbert",
+        location: "London, United Kingdom",
+        image: "/images/image-1.jpg",
+    },
+    {
+        name: "Jordan Okeke",
+        location: "Lupus, Nigaria",
+        image: "/images/image-1.jpg",
+    },
+    {
+        name: "Okere Herbert",
+        location: "London, United Kingdom",
+        image: "/images/image-1.jpg",
+    },
+]
+</script>
+
+
+
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
+
+<!-- :style="{ backgroundImage: `url(${column.image})` }" -->
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,23 @@
 <template>
     <div>
-
+        <div class="p-6">
+            <PersonCard />
+        </div>
     </div>
 </template>
 
 <script>
+import PersonCard from '~/components/common/PersonCard.vue'
+
+export default {
+    components: {
+        PersonCard
+    },
+    data() {
+        return {
+        }
+    }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
